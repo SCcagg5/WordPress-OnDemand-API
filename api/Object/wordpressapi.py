32a4583a2c3553b111e12593c0fb1103a0e4ce36
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-02-18 18:28:36 +0100

    adding save & fromsave

--- api/Object/wordpressapi.py
+++ api/Object/wordpressapi.py
@@ -11,7 +11,7 @@ class wordpress:
         if name is None:
             return [False, "No name provided"]
         save = "/srv/src/" + name
-        base = "/srv/www/"+ self.domain
+        base = "/wordpress/"+ self.domain
         if os.path.isdir(save):
             return [False, "Save name already in use: '" + name + "'" , 400]
         if not os.path.isdir(base):
@@ -24,27 +24,38 @@ class wordpress:
         if name is None:
             return [False, "No name provided"]
         save = "/srv/src/" + name
-        base = "/srv/www/"+ self.domain
+        base = "/wordpress/"+ self.domain
         if not os.path.isdir(save):
             return [False, "Save name does not exist : '" + name + "'" , 400]
         if os.path.isdir(base):
             return [False, "Domain already in use : '" + name + "'" , 400]
         letters = string.ascii_lowercase
         password = ''.join(random.choice(letters) for i in range(10))
+        os.system(' mkdir /wordpress/' + self.domain + ' ;\
+                    cp -r '+ save +'/* /wordpress/' + self.domain + ';\
+                    cp /wordpress/' + self.domain + '/sample.env /wordpress/' + self.domain + '/.env;\
+                    echo "V_VIRTUAL_HOST=' + self.domain + '\nV_DB_CONTAINER=db_' + self.safedomain + '\nV_WP_CONTAINER=wp_' + self.safedomain + '" >> /wordpress/' + self.domain + '/.env ;\
+                    echo -e "define( \'DB_HOST\', \'db_' + self.safedomain + '\');" >> /wordpress/' + self.domain + '/html/wp-config.php ;\
+                    cd /wordpress/' + self.domain + ' && docker-compose up -d;\
+		            chown www-data /wordpress/' + self.domain + '/html -R ;\
+                    docker exec wp_' + self.safedomain + ' curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar ;\
+                    docker exec wp_' + self.safedomain + ' chmod +x wp-cli.phar ;\
+                    docker exec wp_' + self.safedomain + ' mv wp-cli.phar /usr/local/bin/wp-cli ;\
+                    docker exec wp_' + self.safedomain + ' wp-cli --allow-root user create ' + login + ' '+ email +' --role=administrator --user_pass=' + password + ' ;\
+                    docker exec wp_' + self.safedomain + ' wp-cli --allow-root option update home "http://' + self.domain + '" ;\
+                    docker exec wp_' + self.safedomain + ' wp-cli --allow-root option update siteurl "http://' + self.domain + '" ;\
+                    echo -e "php_value memory_limit 300M\nphp_value post_max_size 300M\nphp_value upload_max_filesize 300M" >> /wordpress/' + self.domain + '/html/.htaccess;')
         return [True, {"url": "http://" + self.domain , "user": {"login": login, "password": password, "email": email}}, None]
 
     def new(self):
-        base = "/srv/www/"+ self.domain
+        base = "/wordpress/" + self.domain
         if os.path.isdir(base):
             return [False, "Domain already in use : '" + name + "'" , 400]
         os.system(' cp /srv/src/base_wp/ /wordpress/' + self.domain + ' -r ;\
                     cp /wordpress/' + self.domain + '/sample.env /wordpress/' + self.domain + '/.env;\
                     echo "V_VIRTUAL_HOST=' + self.domain + '\nV_DB_CONTAINER=db_' + self.safedomain + '\nV_WP_CONTAINER=wp_' + self.safedomain + '" >> /wordpress/' + self.domain + '/.env ;\
                     cd /wordpress/' + self.domain + ' && docker-compose up -d;\
-		    chown www-data /wordpress/' + self.domain + '/html -R ;\
-                    docker exec wp_' + self.safedomain + ' curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar ;\
-                    docker exec wp_' + self.safedomain + ' chmod +x wp-cli.phar ;\
-                    docker exec wp_' + self.safedomain + ' mv wp-cli.phar /usr/local/bin/wp-cli ;\
+		            chown www-data /wordpress/' + self.domain + '/html -R ;\
                     echo -e "php_value memory_limit 300M\nphp_value post_max_size 300M\nphp_value upload_max_filesize 300M" >> /wordpress/' + self.domain + '/html/.htaccess;')
         return [True, {"url": "http://" + self.domain}, None]
 
